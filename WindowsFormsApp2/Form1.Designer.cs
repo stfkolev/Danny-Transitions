653e1b28fcb7a1a46c70d34c13cc72da731bcc0a
Stf Kolev <42889703+stfkolev@users.noreply.github.com>
2020-02-16 14:39:27 +0200

    Initial Commit

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
+            this.metroTile1 = new MetroFramework.Controls.MetroTile();
+            this.metroTile2 = new MetroFramework.Controls.MetroTile();
+            this.metroTile3 = new MetroFramework.Controls.MetroTile();
+            this.metroTile4 = new MetroFramework.Controls.MetroTile();
+            this.metroPanel2 = new MetroFramework.Controls.MetroPanel();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroProgressBar1 = new MetroFramework.Controls.MetroProgressBar();
+            this.metroPanel1.SuspendLayout();
+            this.metroPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // metroPanel1
+            // 
+            this.metroPanel1.Controls.Add(this.metroTile4);
+            this.metroPanel1.Controls.Add(this.metroTile3);
+            this.metroPanel1.Controls.Add(this.metroTile2);
+            this.metroPanel1.Controls.Add(this.metroTile1);
+            this.metroPanel1.HorizontalScrollbarBarColor = true;
+            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.HorizontalScrollbarSize = 10;
+            this.metroPanel1.Location = new System.Drawing.Point(23, 63);
+            this.metroPanel1.Name = "metroPanel1";
+            this.metroPanel1.Size = new System.Drawing.Size(754, 172);
+            this.metroPanel1.TabIndex = 0;
+            this.metroPanel1.VerticalScrollbarBarColor = true;
+            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.VerticalScrollbarSize = 10;
+            // 
+            // metroTile1
+            // 
+            this.metroTile1.ActiveControl = null;
+            this.metroTile1.Location = new System.Drawing.Point(3, 3);
+            this.metroTile1.Name = "metroTile1";
+            this.metroTile1.Size = new System.Drawing.Size(119, 79);
+            this.metroTile1.TabIndex = 3;
+            this.metroTile1.Text = "metroTile1";
+            this.metroTile1.UseSelectable = true;
+            // 
+            // metroTile2
+            // 
+            this.metroTile2.ActiveControl = null;
+            this.metroTile2.Location = new System.Drawing.Point(128, 3);
+            this.metroTile2.Name = "metroTile2";
+            this.metroTile2.Size = new System.Drawing.Size(119, 79);
+            this.metroTile2.TabIndex = 4;
+            this.metroTile2.Text = "metroTile2";
+            this.metroTile2.UseSelectable = true;
+            // 
+            // metroTile3
+            // 
+            this.metroTile3.ActiveControl = null;
+            this.metroTile3.Location = new System.Drawing.Point(128, 88);
+            this.metroTile3.Name = "metroTile3";
+            this.metroTile3.Size = new System.Drawing.Size(119, 79);
+            this.metroTile3.TabIndex = 5;
+            this.metroTile3.Text = "metroTile3";
+            this.metroTile3.UseSelectable = true;
+            // 
+            // metroTile4
+            // 
+            this.metroTile4.ActiveControl = null;
+            this.metroTile4.Location = new System.Drawing.Point(3, 88);
+            this.metroTile4.Name = "metroTile4";
+            this.metroTile4.Size = new System.Drawing.Size(119, 79);
+            this.metroTile4.TabIndex = 6;
+            this.metroTile4.Text = "metroTile4";
+            this.metroTile4.UseSelectable = true;
+            // 
+            // metroPanel2
+            // 
+            this.metroPanel2.Controls.Add(this.metroProgressBar1);
+            this.metroPanel2.Controls.Add(this.metroButton1);
+            this.metroPanel2.HorizontalScrollbarBarColor = true;
+            this.metroPanel2.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel2.HorizontalScrollbarSize = 10;
+            this.metroPanel2.Location = new System.Drawing.Point(23, 241);
+            this.metroPanel2.Name = "metroPanel2";
+            this.metroPanel2.Size = new System.Drawing.Size(754, 186);
+            this.metroPanel2.TabIndex = 1;
+            this.metroPanel2.VerticalScrollbarBarColor = true;
+            this.metroPanel2.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel2.VerticalScrollbarSize = 10;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(319, 50);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(138, 31);
+            this.metroButton1.TabIndex = 2;
+            this.metroButton1.Text = "metroButton1";
+            this.metroButton1.UseSelectable = true;
+            // 
+            // metroProgressBar1
+            // 
+            this.metroProgressBar1.Location = new System.Drawing.Point(3, 21);
+            this.metroProgressBar1.Name = "metroProgressBar1";
+            this.metroProgressBar1.Size = new System.Drawing.Size(748, 23);
+            this.metroProgressBar1.TabIndex = 3;
+            this.metroProgressBar1.Value = 58;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.metroPanel2);
+            this.Controls.Add(this.metroPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.metroPanel1.ResumeLayout(false);
+            this.metroPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroPanel metroPanel1;
+        private MetroFramework.Controls.MetroTile metroTile4;
+        private MetroFramework.Controls.MetroTile metroTile3;
+        private MetroFramework.Controls.MetroTile metroTile2;
+        private MetroFramework.Controls.MetroTile metroTile1;
+        private MetroFramework.Controls.MetroPanel metroPanel2;
+        private MetroFramework.Controls.MetroProgressBar metroProgressBar1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,53 @@
+﻿using MetroFramework.Forms;
+using Transitions;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Тук задаваме начална точка, на нашите елементи, за да можем да ги анимираме.
+            // Анимацията работи така: От тази точка, отиваш на точка A по X или Y.
+            // Например: Ако укажем Left или Right, ние ще анимираме по X.
+            // И обратно, ако зададем Top или Bottom, ние ще анимираме по Y.
+            // 
+            // Най-лесно как се прави: Задава се начална точка на X или Y на елемента,
+            // на който искаме да направим анимация, която е равна на ширината или височината
+            // на формата. Ако имам една форма 400 ширина на 600 височина и искам да анимирам по X,
+            // то тогава ще ми трябва текущата точка, където се намира контролът. Ако е на 231 X и 48 Y,
+            // нещата ще изглеждат така:
+            //
+            // this.metroPanel1.Location = new System.Drawing.Point(400, 48);
+            //
+            // Тук запазваме Y, защото искаме само по X да анимираме. Този тип анимация е отдясно наляво.
+            // Ако искаме отляво надясно, тогава ще трябва да зададем координати от сорта на:
+
+            // 0(началната точка на формата спрямо координатната система) - ширината на контролата. Например:
+            // Ако ширината на панела ми е 200, тогава би изглеждало така:
+            //
+            // this.metroPanel1.Location = new System.Drawing.Point(-200, 48);
+            //
+
+            this.metroPanel1.Location = new System.Drawing.Point(787, 63);
+            this.metroPanel2.Location = new System.Drawing.Point(23, 450);
+
+            // Това тук в скобите на new Transition е типа анимация
+            // В скобите на new TransitionType_<Вида на анимацията> е времето, което ще отнеме
+            // да се изпълни дадената анимация
+            var iTransition_Ease = new Transition(new TransitionType_EaseInEaseOut(2000));
+            var iTransition_Accel = new Transition(new TransitionType_Acceleration(2000));
+
+            // Добавяме елемент, който да анимираме. Ако искаме да имаме различни видове анимации
+            // ще си добавим нов вид анимация(transition).
+            iTransition_Ease.add(this.metroPanel1, "Left", 23);
+            iTransition_Accel.add(this.metroPanel2, "Top", 241);
+
+            // Това изпълнява всичките добавени анимации от по-горе. Е хубу де :D няма да е тая анимацията
+            iTransition_Ease.run();
+            iTransition_Accel.run();
+        }
+    }
+}
